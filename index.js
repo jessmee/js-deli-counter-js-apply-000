f4406f22aff3837ab9df84dd22eadbea31914a45
Jessica Yoo <jessmee1234@gmail.com>
2018-06-20 21:45:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ function takeANumber(katzDeliLine, name) {
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
 
+function takeANumber(number) {
+  katzDeliLine.push(number);
+  return "Welcome, You are number " + katzDeliLine.length
+}
+
 function nowServing(katzDeliLine) {
   if(katzDeliLine.length === 0) { 
     return "There is nobody waiting to be served!"
